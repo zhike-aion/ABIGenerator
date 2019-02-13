0b42199d8535f6715c597ec882ff62ca99f94074
Zhike Zhang <zhike@aion.network>
2019-02-13 17:09:34 -0500

    Removed unused System.err redirection.

--- test/MainTest.java
+++ test/MainTest.java
@@ -10,20 +10,16 @@ import static org.junit.Assert.assertEquals;
 
 public class MainTest {
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
     private final PrintStream originalOut = System.out;
-    private final PrintStream originalErr = System.err;
 
     @Before
     public void setUpStreams() {
         System.setOut(new PrintStream(outContent));
-        System.setErr(new PrintStream(errContent));
     }
 
     @After
     public void restoreStreams() {
         System.setOut(originalOut);
-        System.setErr(originalErr);
     }
 
     @Test
